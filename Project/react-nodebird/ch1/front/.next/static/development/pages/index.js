735042025d2ba080acce4fecf32c1ecbb0cc3dd2
LynnDarinKim <darinkim1128@gmail.com>
2020-01-15 23:49:14 -0400

    configuration, next.js routing

---
+++ Project/react-nodebird/ch1/front/.next/static/development/pages/index.js
@@ -0,0 +1,90 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FVolumes%2FDarinKim%2FNSCC%2F4th_Semester_Winter_2020%2FICOM3010%2Ficom3010_self_dk%2FProject%2Freact-nodebird%2Fch1%2Ffront%2Fpages%2Findex.js!./":
+/*!**************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FVolumes%2FDarinKim%2FNSCC%2F4th_Semester_Winter_2020%2FICOM3010%2Ficom3010_self_dk%2FProject%2Freact-nodebird%2Fch1%2Ffront%2Fpages%2Findex.js ***!
+  \**************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var mod = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_5f137288facb1107b491 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_5f137288facb1107b491 */ "dll-reference dll_5f137288facb1107b491"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Volumes/DarinKim/NSCC/4th_Semester_Winter_2020/ICOM3010/icom3010_self_dk/Project/react-nodebird/ch1/front/pages/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Home = function Home() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Hello, Next!");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ 0:
+/*!******************************************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FVolumes%2FDarinKim%2FNSCC%2F4th_Semester_Winter_2020%2FICOM3010%2Ficom3010_self_dk%2FProject%2Freact-nodebird%2Fch1%2Ffront%2Fpages%2Findex.js ***!
+  \******************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FVolumes%2FDarinKim%2FNSCC%2F4th_Semester_Winter_2020%2FICOM3010%2Ficom3010_self_dk%2FProject%2Freact-nodebird%2Fch1%2Ffront%2Fpages%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FVolumes%2FDarinKim%2FNSCC%2F4th_Semester_Winter_2020%2FICOM3010%2Ficom3010_self_dk%2FProject%2Freact-nodebird%2Fch1%2Ffront%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_5f137288facb1107b491":
+/*!*******************************************!*\
+  !*** external "dll_5f137288facb1107b491" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_5f137288facb1107b491;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=index.js.map
