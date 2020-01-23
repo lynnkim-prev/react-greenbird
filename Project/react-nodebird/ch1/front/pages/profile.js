67aba085c0317abfd7c1fa991909de18732e62cd
LynnDarinKim <darinkim1128@gmail.com>
2020-01-23 12:29:43 -0400

    nextjs structure

--- Project/react-nodebird/ch1/front/pages/profile.js
+++ Project/react-nodebird/ch1/front/pages/profile.js
@@ -1,18 +1,9 @@
-import React from "react";
-import AppLayout from "../components/AppLayout"
-import Head from "next/head"
-
+import React from 'react';
 
 const Profile = () => {
   return (
     <>
-    <Head>
-      <title>NodeBird</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.25.3/antd.css" />
-    </Head>
-    <AppLayout>
       <div>프로필</div>
-    </AppLayout>
     </>
   );
 };
