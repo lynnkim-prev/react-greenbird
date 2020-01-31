599946df679f1da2541cd5a0722c3308b3c68d01
LynnDarinKim <darinkim1128@gmail.com>
2020-01-30 21:40:04 -0400

    fixing errors

--- Sandbox/WebGames/3.bulls_and_cows/bulls-and-cows-app/src/NumberBaseball.jsx
+++ Sandbox/WebGames/3.bulls_and_cows/bulls-and-cows-app/src/NumberBaseball.jsx
@@ -13,70 +13,65 @@ const getNumbers = () => { // 숫자 네개를 랜덤하게 뽑는 함수.
 
 
 const NumberBaseball = () => {
-  const [answer, setAnswer] = useState(getNumbers()),
-  const [value, setValue] = useState(""),
-  const [result, setResult] = useState(""),
-  const [tries, setTries] = useState(""),
+  const [answer, setAnswer] = useState(getNumbers());
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const [tries, setTries] = useState([]);
 
 
   const onSubmitForm = (e) => {
     e.preventDefault();
     if(value === answer.join('')) {
-      setTries((prevState) => {
+      setTries(prevState => 
         [...prevState, 
           {
             try: value, 
             result: "홈런!"
           } 
-        ]
-      })
-      setResult("홈런!"),
+        ]);
+      setResult("홈런!");
       alert("게임을 다시 시작합니다.");
-      setValue(""),
-      setAnswer(getNumbers()),
-      setTries([])
+      setValue("");
+      setAnswer(getNumbers());
+      setTries([]);
     } else {
-      const answerArray = value.split("").map((v) => parseInt(v));
+      const answerArray = value.split("").map(v => parseInt(v));
       let strike = 0;
       let ball = 0;
       if(tries.length >= 9) {
-        setResult(`10번 넘게 틀려서 실패. 답은 ${answer.join(",")} 였습니다!`)
+        setResult(`10번 넘게 틀려서 실패. 답은 ${answer.join(",")} 였습니다!`);
         alert("게임을 다시 시작합니다. ");
-        setValue(""),
-        setAnswer(getNumbers()),
-        setTries([])
+        setValue("");
+        setAnswer(getNumbers());
+        setTries([]);
       } else  {
-        for( let i = 0; i < 4; i += 1) {
-          if( answerArray[i] === answer[i] ) {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            console.log("strike", answerArray[i], answer[i]);
             strike += 1;
-          } else if(answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
+            console.log("ball", answerArray[i], answer.indexOf(answerArray[i]));
             ball += 1;
           }
         }
-        setTries((prevState) => {
-          [
+        setTries(prevState => [
             ...prevState, 
             {
               try: value, 
               result: `${strike} 스트라이크, ${ball} 볼입니다`
             }
-          ]
-        });
+          ]);
         setValue("");
       }
     }
   };
-
-  // const onChangeInput = (e) => {
-  //   setValue(e.target.value);
-  // }
 
   return (
     <>
       <h1>{result}</h1>
       <form onSubmit={onSubmitForm}>
         <input
-          ref={inputEl}
+          // ref={inputEl}
           maxLength={4}
           value={value}
           onChange={e => setValue(e.target.value)}
