5251f95d68a415b28caa8f8087e84439b9322d66
LynnDarinKim <darinkim1128@gmail.com>
2020-01-31 11:50:10 -0400

    response check ~

---
+++ Sandbox/WebGames/4.response_check/response-check/src/ResponseCheck-class.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ResponseCheck extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <div>test</div>
+      </>
+    );
+  }
+}
+
+export default ResponseCheck;
