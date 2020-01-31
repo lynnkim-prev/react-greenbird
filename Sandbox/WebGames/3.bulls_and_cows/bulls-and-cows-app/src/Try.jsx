09c9da233e9335392d38c3871a36e9fd6c55291d
LynnDarinKim <darinkim1128@gmail.com>
2020-01-31 02:15:50 -0400

    class <-> hooks usage

--- Sandbox/WebGames/3.bulls_and_cows/bulls-and-cows-app/src/Try.jsx
+++ Sandbox/WebGames/3.bulls_and_cows/bulls-and-cows-app/src/Try.jsx
@@ -1,27 +1,39 @@
 // class: PureComponent, shouldUpdateComponent
-// import React, { PureComponent } from 'react'
-// class Try extends PureComponent {
-//   render() {
-//     const {tryInfo} = this.props;
-//     return (
-//       <li>
-//         <div>{tryInfo.try} </div>
-//         <div>{tryInfo.result} </div>
-//       </li>
-//     );
-//   }
-// }
+import React, { PureComponent } from 'react'
+class Try extends PureComponent {
+  constructor(props) {
+    super(props)
+    // 다른 정밀한 동작
+    const filtered = this.props.filter(() => {
+      
+    });
+    this.state = {
+       result: filtered,
+       try: this.props.try,
+    }
+  }
+
+  render() {
+    const {tryInfo} = this.props;
+    return (
+      <li>
+        <div>{tryInfo.try} </div>
+        <div>{tryInfo.result} </div>
+      </li>
+    );
+  }
+}
 
 
 //hooks : memo
-import React, { memo } from "react";
-const Try = memo(({tryInfo}) => {
-  return (
-    <li>
-      <div>{tryInfo.try} </div>
-      <div>{tryInfo.result} </div>
-    </li>
-  )
-})
+// import React, { memo } from "react";
+// const Try = memo(({tryInfo}) => {
+//   return (
+//     <li>
+//       <div>{tryInfo.try} </div>
+//       <div>{tryInfo.result} </div>
+//     </li>
+//   )
+// })
 
 export default Try;
