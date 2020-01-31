85ee27ae5520ef294f4a58d43203934d6bc8a4a0
LynnDarinKim <darinkim1128@gmail.com>
2020-01-31 12:13:53 -0400

    ResponseCheck ~

--- Sandbox/WebGames/4.response_check/response-check/src/ResponseCheck-class.jsx
+++ Sandbox/WebGames/4.response_check/response-check/src/ResponseCheck-class.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 
 class ResponseCheck extends Component {
-  state = {};
+  state = {
+    state: "waiting",
+    message: "Click to Start",
+    result: []
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    result.length === 0 ? null : result.reduce((a, b) => a + b) / result.length;
+  };
+
+  onClickScreen = () => {};
 
   render() {
+    const { state, message } = this.state;
+
     return (
       <>
-        <div>test</div>
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          <div>{message}</div>
+        </div>
+        {this.renderAverage}
       </>
     );
   }
--- Sandbox/WebGames/4.response_check/response-check/src/index.js
+++ Sandbox/WebGames/4.response_check/response-check/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 import ResponseCheck from './ResponseCheck-class';
 // import ResponseCheck from './ResponseCheck-hook';
+
 
 ReactDOM.render(<ResponseCheck />, document.getElementById('root'));
