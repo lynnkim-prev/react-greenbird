1193623232dfb069005df98cb580c088e5da8ac8
LynnDarinKim <darinkim1128@gmail.com>
2020-02-14 16:10:41 -0400

    Lotto class to hooks

--- Sandbox/WebGames/6.lotto/lotto/src/LottoHooks.jsx
+++ Sandbox/WebGames/6.lotto/lotto/src/LottoHooks.jsx
@@ -1,4 +1,11 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from "react";
+import Ball from "./Ball";
 
 function getWinNumbers() {
   console.log("getWinNumbers");
@@ -11,8 +18,9 @@ function getWinNumbers() {
       candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
     );
   }
+  const bonusNumber = shuffle[shuffle.length - 1];
   const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
-  return [...winNumbers];
+  return [...winNumbers, bonusNumber];
 }
 
 const Lotto = () => {
@@ -21,10 +29,24 @@ const Lotto = () => {
   const [winBalls, setWinBalls] = useState([]);
   const [bonus, setBonus] = useState(null);
   const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
 
   useEffect(() => {
     console.log("useEffect");
-    for (let i = 0; i < winNumbers.length - 1; i++) {}
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      timeouts.current[i] = setTimeout(() => {
+        setWinBalls(prevBalls => [...prevBalls, winNumbers[i]]);
+      }, (i + 1) * 1000);
+    }
+    timeouts.current[6] = setTimeout(() => {
+      setBonus(winNumbers[6]);
+      setRedo(true);
+    }, 7000);
+    return () => {
+      timeouts.current.forEach(v => {
+        clearTimeout(v);
+      });
+    };
   }, [timeouts.current]); // 빈 배열이면 componentDidMount와 동일
   // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
 
@@ -44,15 +66,17 @@ const Lotto = () => {
 
   return (
     <>
-      <div>Winning number!</div>
+      <div>Winning number</div>
       <div id="결과창">
         {winBalls.map(v => (
           <Ball key={v} number={v} />
         ))}
       </div>
       <div>Bonus!</div>
-      {redo && <button onClick={onClickRedo}>One more time?</button>}
+      {bonus && <Ball number={bonus} onClick={onClickRedo} />}
+      {redo && <button onClick={onClickRedo}>한 번 더!</button>}
     </>
   );
 };
+
 export default Lotto;
