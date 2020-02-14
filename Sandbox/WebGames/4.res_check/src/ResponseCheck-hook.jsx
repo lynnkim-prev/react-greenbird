397685d7966ceeb418bec7685cb80db5a0cd6eea
LynnDarinKim <darinkim1128@gmail.com>
2020-02-14 11:38:27 -0400

    response hooks, timeout with useRef

--- Sandbox/WebGames/4.res_check/src/ResponseCheck-hook.jsx
+++ Sandbox/WebGames/4.res_check/src/ResponseCheck-hook.jsx
@@ -1,60 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 
 const ResponseCheck = () => {
   const [state, setState] = useState("waiting");
   const [message, setMessage] = useState("클릭해서 시작하세용");
   const [result, setResult] = useState([]);
+  const timeout = useRef(null);
+  const startTime = useRef(); // 화면을 바꾸고 싶지는 않은데, 값이 바뀌는 것들, 무조건 current로 접근해야 한다
+  const endTime = useRef();
 
-  timeout;
-  startTime;
-  endTime;
-
-  onClickScreen = () => {
+  const onClickScreen = () => {
     if (state === "waiting") {
-      setState("ready");
-      setMessage("기다렸다가 초록색되면 클릭 ㄲㄱ");
-      this.timeout = setTimeout(() => {
+      timeout.current = setTimeout(() => {
         setState("now");
         setMessage("지금 클릭 ㄲ");
-        this.startTime = new Date();
+        startTime.current = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+      setState("ready");
+      setMessage("기다렸다가 초록색되면 클릭 ㄲㄱ");
     } else if (state === "ready") {
-      console.log("땡!!");
-      clearTimeout(this.timeout); // Timeout 초기화. 없으면 땡인데도 2~3초 랜덤 먹음
+      clearTimeout(timeout.current); // Timeout 초기화. 없으면 땡인데도 2~3초 랜덤 먹음
       setState("waiting");
       setMessage("성급했어요ㅉㅉ 다시 클릭해서 시작하시죠??");
-      setResult([]);
     } else if (state === "now") {
-      this.endTime = new Date();
+      endTime.current = new Date();
       setState("waiting");
       setMessage("잘했어요 클릭해서 또 시작하세요!");
-      setResult(prevState => [
-        ...prevState.result,
-        this.endtime - this.startTime
-      ]);
+      setResult(prevResult => {
+        return [...prevResult, endTime.current - startTime.current];
+      });
       console.log("굿잡");
     }
   };
 
-  onReset = () => {
+  const onReset = () => {
     setResult([]);
   };
 
-  renderAverage = () => {
+  const renderAverage = () => {
     return result.length === 0 ? null : ( // 삼항연산자. result가 없어서 array에 보여줄게 없을 때 null로 처리해서 안보이게 함
       <>
         <div>평균시간:{result.reduce((a, c) => a + c) / result.length}ms</div>
-        <button onClick={this.onReset}>리셋ㄲ</button>
+        <button onClick={onReset}>리셋ㄲ</button>
       </>
     );
   };
 
   return (
     <>
-      <div id="screen" className={state} onClick={this.onClickScreen}>
+      <div id="screen" className={state} onClick={onClickScreen}>
         {message}
       </div>
-      {this.renderAverage()}
+      {renderAverage()}
     </>
   );
 };
--- Sandbox/WebGames/4.res_check/src/index.js
+++ Sandbox/WebGames/4.res_check/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ResponseCheck from "./ResponseCheck-class";
-// import ResponseCheck from './ResponseCheck-hook';
+// import ResponseCheck from "./ResponseCheck-class";
+import ResponseCheck from "./ResponseCheck-hook";
 
 ReactDOM.render(<ResponseCheck />, document.getElementById("root"));
+
+
+
+
