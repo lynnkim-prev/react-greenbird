8c9fa444aaf41abb29d6442505f9c3c7f003ada5
LynnDarinKim <darinkim1128@gmail.com>
2020-02-20 01:30:43 -0400

    with comments

--- Sandbox/WebGames/7.tic_tac_toe/tic_tac_toe/src/TicTacToe.jsx
+++ Sandbox/WebGames/7.tic_tac_toe/tic_tac_toe/src/TicTacToe.jsx
@@ -11,8 +11,8 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_WINNER':
       return {
-        ...state,
-        winner: action.winner,
+        ...state, // state.winner = action.winner 하면 안됨! 항상 새로운 객체를 만들어줘야 함
+        winner: action.winner, // 기존 state를 복사해놓고 + 바뀌는 부분(winner)만 바꿔주기
       };
   }
 };
@@ -24,6 +24,10 @@ const TicTacToe = () => {
   // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], , ['', '', '']]);
 
   const onClickTable = useCallback(() => {
+    // dispatch - action. dispatch안에 들어가는건 action이라고 부름. action object.
+    // dispatch한다 = action을 실행한다. conduct action
+    // 액션만 해서는 자동으로 state가 바뀌는게 아니고, action을 해석해서 state를 해석해서 직접 바꿔주는 역할이 필요하다 (reducer)
+    // 액션을 dispatch -> reducer 실행
     dispatch({ type: 'SET_WINNER', winner: '0' });
   }, []);
 
