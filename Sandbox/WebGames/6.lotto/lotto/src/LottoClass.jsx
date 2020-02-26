58da3bdfef8481cede2171169b5ec3726243b2f8
LynnDarinKim <darinkim1128@gmail.com>
2020-02-26 19:01:38 -0400

    Lotto in progress

--- Sandbox/WebGames/6.lotto/lotto/src/LottoClass.jsx
+++ Sandbox/WebGames/6.lotto/lotto/src/LottoClass.jsx
@@ -1,27 +1,53 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Ball from './Ball';
 
-function getWinNumbers() {
-  console.log(getWinNumbers);
+function selectedNumbers() {
+  console.log('selectedNumbers');
+
   const candidate = Array(45)
     .fill()
-    .map((v, i) => i + 1);
+    .map((v, i) => {
+      return i + 1;
+    });
+
   const shuffle = [];
   while (candidate.length > 0) {
     shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
   }
 
-  const bonusNumber = shuffle[shuffle.length - 1];
-  const winNumbers = shuffle.splice(0, 6).sort((p, c) => p - c);
-  return [...winNumbers, bonusNumber];
+  const winningNumber = shuffle.slice(0, 6).sort((a, b) => {
+    return a - b;
+  });
+
+  const bonusNumber = shuffle.pop();
+
+  return { winningNumber: winningNumber, bonusNumber: bonusNumber };
 }
 
 class Lotto extends Component {
-  state = {};
+  state = {
+    selectedNumberArr: selectedNumbers(),
+    winNumberArr: selectedNumbers().winningNumber,
+    bonusNumber: selectedNumbers().bonusNumber,
+    redo: false,
+  };
+
+  onClickRedo = () => {};
 
   render() {
-
+    const { winNumberArr, bonusNumber, redo } = this.state;
     return (
-
+      <>
+        <p>winning numbers</p>
+        <div id='resultScreen'>
+          {winNumberArr.map(v => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <p>Bonus number</p>
+        {bonusNumber && <Ball number={bonusNumber} />}
+        <button onClick={redo ? this.onClickRedo : () => {}}>one more time? </button>
+      </>
     );
   }
 }
