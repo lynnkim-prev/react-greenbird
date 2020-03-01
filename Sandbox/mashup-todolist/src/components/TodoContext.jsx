3c4cb392d41ff49b68dbb5afb66736a296e03fe8
LynnDarinKim <darinkim1128@gmail.com>
2020-03-01 13:46:08 -0400

    todo App in progress (Context API + useReducer)

---
+++ Sandbox/mashup-todolist/src/components/TodoContext.jsx
@@ -0,0 +1,31 @@
+import React, { useReducer } from "react";
+
+const initialTodos = [
+  {
+    id: 1,
+    text: "Create a project",
+    done: true
+  },
+  {
+    id: 2,
+    text: "Style components",
+    done: true
+  },
+  {
+    id: 3,
+    text: "Yawn",
+    done: true
+  },
+  {
+    id: 4,
+    text: "Create context",
+    done: false
+  },
+  {
+    id: 5,
+    text: "Build functionalities'",
+    done: false
+  }
+];
+
+
